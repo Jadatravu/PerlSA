5270fdf1079ab01f94300681a57bd7a6037f217f
Janardhana Adatravu <surya.janardhan@gmail.com>
2015-09-10 14:38:57 +0530

    new template tag 'change_file_path' implemented

--- SaProj/SaApp/templatetags/ttags.py
+++ SaProj/SaApp/templatetags/ttags.py
@@ -3,6 +3,11 @@ from django import template
 register = template.Library()
 
 @register.simple_tag(takes_context = True)
+def change_file_path (context,path_str):
+    res_str=str(path_str).replace('^','/')
+    return res_str
+
+@register.simple_tag(takes_context = True)
 def add_builds_table(context,dic):
     res_str=str('')
     build_dic_list = dic
@@ -18,6 +23,6 @@ def add_builds_table(context,dic):
                   build_issue_link = str("<a>None</a>")
               else:
                   build_issue_link = str("<a href=/issue_bet_builds/?build1=")+str(earlier_build_name) + str("&build2=") + str(value["name"]) + str("> issues link</a>")
-              res_str += str("<tr><td></td><td>") + value["name"] + str("</td><td>") + str(value["issues"]) + str("</td><td>") + str(build_issue_link)+str("</td></tr>")
+              res_str += str("<tr><td></td><td>") + value["name"] + str("</td><td>")+str(value['revision']) +str("</td><td>") + str(value["issues"]) + str("</td><td>") + str(build_issue_link)+str("</td></tr>")
               earlier_build_name = value["name"]
     return res_str
